870e70daee60d086505dd35e9cf04f69200a4d9c
Tessa Thornton <tessathornton@gmail.com>
2025-01-17 12:59:35 -0500

    quotes for replies

--- src/webmentions.js
+++ src/webmentions.js
@@ -11,7 +11,7 @@
         let author = child.author.name.length > 0 ? child.author.name: child.author.url;
         const li = el.appendChild(document.createElement('li'));
         if (child['wm-property'] === "in-reply-to") {
-          li.innerHTML = `<p><a href="${child.url}">${author} replied ${child.content.text}</a>`
+          li.innerHTML = `<p><a href="${child.url}">${author} replied \"${child.content.text}\"</a>`
         } else if (child['wm-property'] == 'like-of') {
           li.innerHTML = `<p><a href="${child.url}">${author} liked this</a>`   
         } else if (child['wm-property'] == 'repost-of') {
