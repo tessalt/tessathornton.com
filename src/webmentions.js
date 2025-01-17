b2529e59b41dfac24cf027829680f88fc621fda5
Tessa Thornton <tessathornton@gmail.com>
2025-01-17 12:52:26 -0500

    display mentions

---
+++ src/webmentions.js
@@ -0,0 +1,20 @@
+(() => {
+  
+  const url = window.location.href;
+  // const url = "https://tessathornton.com/posts/first-post.html";
+  const el = document.getElementById("mentions");
+
+  fetch("https://webmention.io/api/mentions.jf2?target=" + url)
+    .then(response => response.json())
+    .then((json) => {
+      json.children.forEach((child) => {
+        let author = child.author.name.length > 0 ? child.author.name: child.author.url;
+        const li = el.appendChild(document.createElement('li'));
+        if (child['wm-property'] === "in-reply-to") {
+          li.innerHTML = `<p><a href="${child.url}">${author} replied ${child.content.text}</a>`
+        } else if (child['wm-property'] == 'like-of') {
+          li.innerHTML = `<p><a href="${child.url}">${author} liked this</a>`   
+        }
+      })
+    }); 
+})();
